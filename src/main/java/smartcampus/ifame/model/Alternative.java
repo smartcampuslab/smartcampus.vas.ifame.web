0f1d74e29d6c3a9fc20bc0cf0626680ee61ece60
francesco <franz@franz-dell>
2013-05-29 14:32:34 +0200

    menu alternative added

---
+++ src/main/java/smartcampus/ifame/model/Alternative.java
@@ -0,0 +1,18 @@
+package smartcampus.ifame.model;
+
+import java.util.List;
+
+public class Alternative {
+	private List<PiattoKcal> alternative;
+
+	public Alternative() {
+	}
+
+	public List<PiattoKcal> getAlternative() {
+		return alternative;
+	}
+
+	public void setAlternative(List<PiattoKcal> alternative) {
+		this.alternative = alternative;
+	}
+}
