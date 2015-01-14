7b21c21922f847040bc3c17cd60e77ca19370871
raman <raman@fbk.eu>
2015-01-14 13:29:58 +0100

    mensa container

---
+++ src/main/java/eu/trentorise/smartcampus/vas/ifame/model/MensaContainer.java
@@ -0,0 +1,16 @@
+package eu.trentorise.smartcampus.vas.ifame.model;
+
+import java.util.List;
+
+public class MensaContainer {
+
+	private List<Mensa> mense;
+
+	public List<Mensa> getMense() {
+		return mense;
+	}
+
+	public void setMense(List<Mensa> mense) {
+		this.mense = mense;
+	}
+}
