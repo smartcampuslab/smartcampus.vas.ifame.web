a7adc02cdee23d4a5ad1434c7e71fcf6f381177a
Mattia Bortolamedi <regolo85@gmail.com>
2015-12-23 17:59:25 +0100

    Updated files for new menu of January 2016

--- src/main/java/eu/trentorise/smartcampus/vas/ifame/controllers/MenuController.java
+++ src/main/java/eu/trentorise/smartcampus/vas/ifame/controllers/MenuController.java
@@ -194,7 +194,7 @@ public class MenuController {
 		logger.debug("Inizializzazione database");
 
 		Workbook workbook = NewMenuXlsUtil.getWorkbook(getClass()
-				.getResourceAsStream("/dicembre2015.xls"));
+				.getResourceAsStream("/gennaio2016.xls"));
 
 		GestoreMenu.inizializzazioneMenuDatabase(piattoGiornoRepo,
 				piattoRepository, workbook);
