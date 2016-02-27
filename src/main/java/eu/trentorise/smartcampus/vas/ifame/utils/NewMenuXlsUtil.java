23daada8344bed965872aa99f5f4432d80b510d9
Mattia Bortolamedi <regolo85@gmail.com>
2016-02-27 23:19:48 +0100

    Updated new menu for march

--- src/main/java/eu/trentorise/smartcampus/vas/ifame/utils/NewMenuXlsUtil.java
+++ src/main/java/eu/trentorise/smartcampus/vas/ifame/utils/NewMenuXlsUtil.java
@@ -34,7 +34,7 @@ public class NewMenuXlsUtil {
 	// ***************************************************************
 	// IMPORTANTE: DA CAMBIARE IN BASE AL NUMERO DI SETTIMANE DEL MESE
 	// IL FOGLIO FINALE
-	private static final int FOGLIO_FINALE = 5; //febbraio
+	private static final int FOGLIO_FINALE = 5; //marzo
 	private static final int FOGLIO_INIZIALE = 0;
 	// ---------------------------------------------------------------
 	// RIGHE E COLONNE DEI MENU
