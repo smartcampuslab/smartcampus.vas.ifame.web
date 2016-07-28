53972b161ff7659ac372eb12afebdea4aa98b8fa
Mattia Bortolamedi <regolo85@gmail.com>
2016-07-28 09:56:19 +0200

    Updated files and data for new month menu (agosto 2016)

--- src/main/java/eu/trentorise/smartcampus/vas/ifame/controllers/MenuController.java
+++ src/main/java/eu/trentorise/smartcampus/vas/ifame/controllers/MenuController.java
@@ -194,7 +194,7 @@ public class MenuController {
 		logger.debug("Inizializzazione database");
 
 		Workbook workbook = NewMenuXlsUtil.getWorkbook(getClass()
-				.getResourceAsStream("/luglio2016.xls"));
+				.getResourceAsStream("/agosto2016.xls"));
 
 		GestoreMenu.inizializzazioneMenuDatabase(piattoGiornoRepo,
 				piattoRepository, workbook);
